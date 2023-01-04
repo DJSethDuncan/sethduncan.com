d9245e52242e91b4a274bbf98e732ad68ace27f3
Seth Duncan <sethduncan@gmail.com>
2023-01-03 21:48:28 -0600

    new layout

--- 2022-12/src/components/App.js
+++ 2022-12/src/components/App.js
@@ -7,12 +7,42 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="header">
-          <p class="headerTitle">Seth Duncan</p>
+          <div class="titleContainer">
+            <p class="headerTitle">Seth Duncan</p>
+          </div>
+
+          <div class="titleLinkList">
+            <a href="https://github.com/DJSethDuncan" target="_new">
+              CODE
+            </a>
+            <a href="https://sethduncanmusic.bandcamp.com/" target="_new">
+              MUSIC
+            </a>
+          </div>
         </header>
-        <div className="body">
-          {/* <Logo /> */}
-          <Links />
+        <div class="separator">
+          <hr />
         </div>
+        <div id="aboutText">
+          <p>
+            <strong>I'm a creator and a tinkerer.</strong> At my happiest, I'll
+            be found making something new to be put out in the world. Most often
+            this shows through building software and writing music.
+          </p>
+          <p>
+            <strong>I love collaborating.</strong> I reach for the moments where
+            I discover something new by working with other people.
+          </p>
+          <p>
+            <strong>I'm a perpetual student.</strong> Some more information
+            about this thing.
+          </p>
+        </div>
+
+        {/* <div className="body"> */}
+        {/* <Logo /> */}
+        {/* <Links /> */}
+        {/* </div> */}
       </div>
     </div>
   );
