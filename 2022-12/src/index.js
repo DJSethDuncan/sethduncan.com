96306e74ddf9df81144766f2eb3a9f0c40d10226
Seth Duncan <sethduncan@gmail.com>
2023-10-16 09:55:12 -0500

    add https redirect package

--- 2022-12/src/index.js
+++ 2022-12/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import App from "./components/App";
+import HttpsRedirect from "react-https-redirect";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HttpsRedirect>
+      <App />
+    </HttpsRedirect>
   </React.StrictMode>
 );
