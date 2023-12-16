297c52b5b231ac04de3a517ef86036a4fb79f8ec
Seth Duncan <sethduncan@gmail.com>
2023-12-16 01:51:48 -0600

    rel external

--- 2022-12/src/components/IndividualLink.js
+++ 2022-12/src/components/IndividualLink.js
@@ -1,7 +1,11 @@
 import "../styles/IndividualLink.css";
 export default function IndividualLink(props) {
   return (
-    <a href={props.link} target={props.external ? "_new" : "_self"}>
+    <a
+      href={props.link}
+      rel="external"
+      target={props.external ? "_new" : "_self"}
+    >
       {props.text}
     </a>
   );
