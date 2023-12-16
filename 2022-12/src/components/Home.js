ade961ee881343b2b8c5ad8ff994fc6d571f3ebe
Seth Duncan <sethduncan@gmail.com>
2023-12-16 01:30:06 -0600

    add landon

---
+++ 2022-12/src/components/Home.js
@@ -0,0 +1,38 @@
+export default function Home() {
+  return (
+    <>
+      <header className="header">
+        <div class="titleContainer">
+          <p class="headerTitle">Seth Duncan</p>
+        </div>
+
+        <div class="titleLinkList">
+          <a href="https:github.com/DJSethDuncan" target="_new">
+            CODE
+          </a>
+          <a href="https:sethduncanmusic.bandcamp.com/" target="_new">
+            MUSIC
+          </a>
+        </div>
+      </header>
+      <div class="separator">
+        <hr />
+      </div>
+      <div id="aboutText">
+        <p>
+          <strong>I'm a creator and a tinkerer.</strong> At my happiest, I'll be
+          found making something new to be put out in the world. Most often this
+          shows through building software and writing music.
+        </p>
+        <p>
+          <strong>I love collaborating.</strong> I reach for the moments where I
+          discover something by working with other people.
+        </p>
+        <p>
+          <strong>I'm a perpetual student.</strong> I can't stop won't stop
+          learning.
+        </p>
+      </div>
+    </>
+  );
+}
